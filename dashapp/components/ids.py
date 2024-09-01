c12ccf0bdb12a6b0e13d2dd069f4753942577bb3
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-09-01 08:36:45 +0200

    centralizing all unique ids

---
+++ dashapp/components/ids.py
@@ -0,0 +1,33 @@
+# dashapp/components/ids.py
+
+# BUTTONS
+REFRESH_DATA_BUTTON = "refresh-data-button"
+OVERVIEW_CURRENT_MONTH_BUTTON = "overview-current-month-button"
+OVERVIEW_CLEAR_BUTTON = "overview-clear-button"
+OVERVIEW_SELECT_ALL_BUTTON = "overview-select-all-button"
+
+# BUTTON GROUPS
+MONTH_FILTERING_BUTTON_GROUP = "month-filtering-button-group"
+
+# CHECKLISTS
+MONTH_CHECKLIST = "month-checklist"
+YEAR_CHECKLIST = "year-checklist"
+SPORT_TYPE_CHECKLIST = "sport-type-checklist"
+
+# DROPDOWNS
+YEAR_DROPDOWN = "year-dropdown"
+
+# PLOTS
+OVERVIEW_DURATION_BAR_CHART = "overview-duration-bar-chart"
+OVERVIEW_DISTANCE_BAR_CHART = "overview-distance-bar-chart"
+OVERVIEW_ACTIVITY_COUNT_PIE_CHART = "overview-activity-count-pie-chart"
+OVERVIEW_EFFORT_SCATTER_CHART = "overview-effort-scatter-chart"
+OVERVIEW_POLAR_CHART = "overview-radar-chart"
+
+BIKE_STATS_AVERAGE_SPEED_LINE_CHART = "bike-stats-average-speed-line-chart"
+
+# GENERAL VALUES
+ACTIVITY_COUNT_MONTH = "month-activity-count"
+SUFFER_SCORE_MONTH = "month-suffer-score-sum"
+DISTANCE_MONTH = "month-distance-sum"
+DURATION_MONTH = "month-duration-sum"
