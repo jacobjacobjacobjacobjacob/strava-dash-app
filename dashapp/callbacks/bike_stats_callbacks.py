6e98e75990111a0c3b5fd176e446de7af3590ce9
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-09-03 07:15:01 +0200

    housekeeping

--- dashapp/callbacks/bike_stats_callbacks.py
+++ dashapp/callbacks/bike_stats_callbacks.py
@@ -1,4 +1,4 @@
-# dashapp/callbacks/overview_callbacks.py
+# dashapp/callbacks/bike_stats_callback.py
 from dash import Input, Output, State, Dash, callback_context
 import pandas as pd
 
@@ -18,23 +18,37 @@ def register_bike_stats_callbacks(app: Dash, df: pd.DataFrame):
         [
             Output(YEAR_DROPDOWN, "value"),
             Output(BIKE_STATS_AVERAGE_SPEED_LINE_CHART, "figure"),
+            Output(BIKE_STATS_AVERAGE_POWER_LINE_CHART, "figure"),
+            Output(BIKE_STATS_AVERAGE_HR_LINE_CHART, "figure"),
         ],
         [Input(YEAR_DROPDOWN, "value")],
     )
     def update_values(selected_year):
-        # Filter data for 'Bike' sport type
-        filtered_df = df[df["sport_type"] == "Bike"]
-
-        # Apply year filter if selected_year is provided
-        if selected_year:
-            filtered_df = filtered_df[filtered_df["year"] == int(selected_year)]
-
-        # Generate the line chart with the filtered data
+        # Generate the line charts with the filtered data directly
         bike_stats_average_speed_line_chart = get_average_metric_line_chart(
-            df=filtered_df,
-            selected_year=selected_year,
-            sport_type="Bike",
+            df=df,
             metric="average_speed",
+            sport_type="Bike",
+            selected_year=selected_year,
         )
 
-        return (selected_year, bike_stats_average_speed_line_chart)
+        bike_stats_average_power_line_chart = get_average_metric_line_chart(
+            df=df,
+            metric="average_watts",
+            sport_type="Bike",
+            selected_year=selected_year,
+        )
+
+        bike_stats_average_hr_line_chart = get_average_metric_line_chart(
+            df=df,
+            metric="average_heartrate",
+            sport_type="Bike",
+            selected_year=selected_year,
+        )
+
+        return (
+            selected_year,
+            bike_stats_average_speed_line_chart,
+            bike_stats_average_power_line_chart,
+            bike_stats_average_hr_line_chart,
+        )
